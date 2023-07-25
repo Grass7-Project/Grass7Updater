ce4049ddf4fa7746fb3c68bad89db2e64ad77ffe
Genki <86833542+Genki-jp0@users.noreply.github.com>
2023-07-25 17:56:41 +0200

    Fix memory leak

--- FileManagement.cpp
+++ FileManagement.cpp
@@ -8,14 +8,16 @@
 
 int extract(const char *filename)
 {
+	wchar_t *archivefile = gr7::convertchar(filename);
 	try {
 
 		bit7z::Bit7zLibrary lib{
 			_T("7za.dll")
 		};
 		bit7z::BitExtractor extractor{ lib, bit7z::BitFormat::SevenZip };
-
+		
 		extractor.extract(gr7::convertchar(filename), _T("./"));
+		memset(archivefile, 0, sizeof(archivefile));
 	}
 	catch (const bit7z::BitException& ex) {
 		ex.getErrorCode();
